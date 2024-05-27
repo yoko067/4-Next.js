0e1d288952a5ca931e1259959c996be5b99dce23
yoko067 <ee217067@meiji.ac.jp>
2024-05-27 14:14:15 +0900

    <add>: アプリケーションに二つのカスタムフォントを追加

--- tutorial-next/nextjs-dashboard/app/ui/invoices/status.tsx
+++ tutorial-next/nextjs-dashboard/app/ui/invoices/status.tsx
@@ -6,7 +6,7 @@ export default function InvoiceStatus({ status }: { status: string }) {
     <span
       className={clsx(
         'inline-flex items-center rounded-full px-2 py-1 text-xs',
-        {
+        { // 条件付き(statusの値)でクラスを変更
           'bg-gray-100 text-gray-500': status === 'pending',
           'bg-green-500 text-white': status === 'paid',
         },
