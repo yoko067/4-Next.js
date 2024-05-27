ab45df74968957308dbb9712a09cf64d79a2a477
yoko067 <ee217067@meiji.ac.jp>
2024-05-27 16:04:23 +0900

    <fix>: <a>タグをLinkコンポーネントに置き換え、一部のページを更新するだけでページを遷移する事が出来るように変更

--- tutorial-next/nextjs-dashboard/app/layout.tsx
+++ tutorial-next/nextjs-dashboard/app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="ja">
       <body className={`${inter.className} antialiased`/* フォントを滑らかにするクラス */}>{children}</body>
     </html>
   );
